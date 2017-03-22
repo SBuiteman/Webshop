37b8331efb0e4c4a53bf7619fd9951a2f3652f99
Buiteman <stijn.buiteman@sogeti.com>
2017-03-22 12:41:17 +0100

    angular en html veranderingen

--- src/main/webapp/app/controllers/controllers.js
+++ src/main/webapp/app/controllers/controllers.js
@@ -1,3 +1,13 @@
 /**
  * Created by pnederlo on 16-3-2017.
  */
+
+var app = angular.module('webshop.controllers', []);
+
+app.controller('ProductListCtrl', ['$scope', 'UserFactory', function ($scope, UserFactory) {
+    UserFactory.get({}, function (userFactory) {
+        $scope.products = userFactory;
+
+        $log(userFactory);
+    })
+}]);
